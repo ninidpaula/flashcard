6a892a40a7856a0471c8d525e1b0ad43e55a238d
ninidpaula <163007668+ninidpaula@users.noreply.github.com>
2024-10-07 11:33:48 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,47 @@
+criaCartao(
+    'Programação 2.0',
+    'O que é phyton?',
+    'O phyton é uma linguagem de programação'
+)
+
+criaCartao(
+    'Nicolly',
+    'A Ni gosta de assistir o mount?',
+    'Sim'
+)
+
+criaCartao(
+    'Nicolly',
+    'Qual é a comida favorita da Ni?',
+    'É strogonoff'
+)
+
+criaCartao(
+    'Nicolly',
+    'Qual creme a Ni mais gosta de usar?',
+    'O da Hidramais com cheiro de morango'
+)
+
+criaCartao(
+    'Nicolly',
+    'Qual a bebida favorida da Ni?',
+    'Água'
+)
+
+criaCartao(
+    'Nicolly',
+    'Qual lugar a Ni mais gosta de ir?',
+    'A praia'
+)
+
+criaCartao(
+    'Nicolly',
+    'Qual o chocolate favorito da Ni?',
+    'Milka de amendoas'
+)
+
+criaCartao(
+    'Nicolly',
+    'Quais são as materias favoritas da Ni?',
+    'Matematica, biologia e historia'
+)
